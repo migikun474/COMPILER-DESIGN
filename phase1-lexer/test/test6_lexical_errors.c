c40d287fcbcfda16ebb11a7f5c35e77fa25506af
Compiler Project <dev@example.com>
2026-08-15 19:24:37 +0000

    final lexical phase code along with tokenisation for each keyword and identifier for further phases

--- phase1-lexer/test/test6_lexical_errors.c
+++ phase1-lexer/test/test6_lexical_errors.c
@@ -1,12 +1,4 @@
-int main() {
-    char bad = 'a;
-    char *s = "oops;
-    int y = 5 @ 3;
-    char multi = 'AB';
-    char badesc = '\q';
-    char *badhex = "\xz";
-    char *badoct = "\89";
-    int badoctnum = 089;
-    int badbinnum = 0b1102;
-    /* this comment never closes
-    int z = 1;
+int main(){
+    char *s = "hello
+world";
+}
