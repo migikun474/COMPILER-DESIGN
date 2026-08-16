d3936ce31d11f4dea906fdf3bf3ffcc992b9fc2f
Amirishetty Sai Vignesh <vignesh_as@cs.iitr.ac.in>
2026-08-16 17:58:56 +0530

    Introduce lexical error test cases
    Added various lexical errors to test cases including invalid characters and formats.

--- phase1-lexer/test/test6_lexical_errors.c
+++ phase1-lexer/test/test6_lexical_errors.c
@@ -1,3 +1,14 @@
-int main(){
-   int a 1 2 3 = 5 ; 
-}
+int main() {
+    char bad = 'a;
+    char *s = "oops;
+    int y = 5 @ 3;
+    char multi = 'AB';
+    char badesc = '\q';
+    char *badhex = "\xz";
+    char *badoct = "\89";
+    int badoctnum = 089;
+    int badbinnum = 0b1102;
+    int barehex = 0x;
+    int barebin = 0b;
+    /* this comment never closes
+    int z = 1;
